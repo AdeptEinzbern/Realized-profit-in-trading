5a1295de168bb258a2a44782166a9fef680e37d1
Adept von Einzbern <72400857+AdeptEinzbern@users.noreply.github.com>
2023-02-10 18:08:41 +0700

    Add files via upload

--- fifo01.py
+++ fifo01.py
@@ -85,6 +85,7 @@ while(i >= 0):
             print("stock_amount after sell = ", stock_amount[j])
             print("stock_amount02 after sell = ", stock_amount[j-1])
             print("\n")
+            
         if size[i] == stock_amount[j] and price[i] > stock_price[j]:
             print("size == stock amount")
             print("size = ", size[i])
@@ -99,6 +100,75 @@ while(i >= 0):
             print("realised profit = ", profit)
             print("i = ",i)
             print("\n")
+
+        if price[i] < stock_price[j]:
+            print("price < stock price")
+            print("price < stock price")
+            print("price < stock price")
+            print("price < stock price")
+            print("price < stock price")
+            print("price < stock price")
+            print("price < stock price")
+            print("price < stock price")
+            print("price < stock price")
+            print("price < stock price")
+            print("j = ", j)
+            print("i = ", i)
+            j2 = j
+            while(i >= 0):
+                print("running loop find price[i] < stock_price[j]")
+                if price[i] > stock_price[j2]:
+                    print("Found price[i] > stock_price")
+                    print("stock price = ", stock_price[j2])
+                    print("price = ", price[i])
+
+
+                    if size[i] < stock_amount[j2] and stock_amount[j2] != 9999: #stock_amount 9999 is no more stock left at this array
+                        print("size < stock amount")
+                        print("size = ", size[i])
+                        print("at price", price[i])
+                        print("stock price = ", stock_price[j2])
+                        print("stock amount = ", stock_amount[j2])
+                        real = (price[i]*size[i]) - (size[i] * stock_price[j2])
+                        profit = real + profit
+                        print("real = ", real)
+                        stock_amount[j] = stock_amount[j] - size[i]  ### reduce stock[j] because sold out for size[i]
+                        print("stock_amount after sell = ", stock_amount[j])
+                        print("\n")
+                    if size[i] > stock_amount[j2] and stock_amount[j2] != 9999:
+                        print("size > stock amount")
+                        print("size = ", size[i])
+                        print("at price", price[i])
+                        print("stock price = ", stock_price[j2])
+                        print("stock amount = ", stock_amount[j2])
+                        real = (price[i]*size[i]) - (size[i] * stock_price[j2])
+                        profit = real + profit
+                        print("real01 = ", real)
+                        temp = size[i] - stock_amount[j2]
+                        print("over size = ", temp)
+                        stock_amount[j2] = 9999 ## we don't need to use this array anymore
+                        print("find next stock_amount")
+                       
+                        
+                    if size[i] == stock_amount[j]:
+                        print("size == stock amount")
+                        print("size = ", size[i])
+                        print("at price", price[i])
+                        print("stock price = ", stock_price[j])
+                        print("stock amount = ", stock_amount[j])
+                        real = (price[i]*size[i]) - (size[i] * stock_price[j])
+                        print("real = ", real)
+                        profit = real + profit
+                        stock_amount[j] = 9999
+                        j = j - 1
+                        print("realised profit = ", profit)
+                        print("i = ",i)
+                        print("\n")
+
+
+                    
+                    break
+                j2 = j2 - 1
         print("profit = ", profit)
     
     i = i - 1
@@ -116,6 +186,7 @@ print("profit = ", profit)
 print("net profit = ", profit - fee_main)
 print("\n")
 ##show order left
+
 i = temp
 while(i >= 0):
     
@@ -123,4 +194,4 @@ while(i >= 0):
         print("stock buy amount",stock_amount[i])
         print("stock price ", stock_price[i])
         print("i = ", i)
-        i = i - 1
+    i = i - 1
